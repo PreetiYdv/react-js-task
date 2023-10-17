af9f1f6ca50f8c19e058c56b3b090b6edb487a06
PreetiYdv <preetiyadav5443@gmail.com>
2023-10-17 21:24:38 +0530

    form and range exponent

--- src/components/RangeExponentData.jsx
+++ src/components/RangeExponentData.jsx
@@ -27,39 +27,40 @@ const RangeExponentData = () => {
         <div className="container mx-auto h-full flex justify-center flex-col items-center">
           <form onSubmit={handleSubmit}>
             <div className="flex flex-col justify-center rounded-md border bg-white/5 backdrop-blur-md  gap-5 px-20 py-10">
-              <div className="mb-5 flex gap-2">
+              <div className="flex gap-2">
                 <input
                   type="text"
-                  className="p-1"
+                  className="py-1 px-2 bg-transparent text-white border-b outline-none appearance-none"
                   placeholder="starting number"
                   value={start}
                   onChange={(e) => setStart(e.target.value)}
                 />
                 <input
                   type="text"
-                  className="p-1"
+                  className="py-1 px-2 bg-transparent text-white border-b outline-none appearance-none"
                   placeholder="end"
                   value={end}
                   onChange={(e) => setEnd(e.target.value)}
-                />
-                <input
-                  type="text"
-                  className="p-1"
-                  placeholder="end"
-                  value={power}
-                  onChange={(e) => setPower(e.target.value)}
-                />
+                />{" "}
               </div>
+              <input
+                type="text"
+                className="py-1 px-2 bg-transparent text-white border-b outline-none appearance-none"
+                placeholder="power"
+                value={power}
+                onChange={(e) => setPower(e.target.value)}
+              />
+
               <button
                 type="submit"
-                className="border py-1 px-4 text-lg text-white rounded-xl"
+                className="border py-1 px-4 text-lg text-white rounded-xl my-5"
               >
                 Output
               </button>
               <div className="mt-5">
                 {data.map((obj, i) => {
                   return (
-                    <span className="text-white mt-5 text-2xl" key={i}>
+                    <span className="text-white text-2xl" key={i}>
                       <span className="mx-2">{obj}</span>
                     </span>
                   );
