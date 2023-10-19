8299248c46f46ff60dbda8d40d65137dbccefa2a
PreetiYdv <preetiyadav5443@gmail.com>
2023-10-19 20:16:24 +0530

    Find vowel

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -5,18 +5,17 @@ import { ContentData } from "./common/Helper";
 const Content = () => {
   return (
     <div className="min-h-screen bg-slate-900 flex justify-center items-center">
-      <div className="flex flex-wrap gap-2">
+      <div className="flex flex-wrap flex-col gap-2">
         {ContentData.map((obj, i) => {
           const { title, link } = obj;
           return (
-           <div  key={i}>
             <Link
+              key={i}
               to={link}
-              className="border-2 rounded-md border-white bg-transparent text-white hover:bg-white hover:text-black transition-all duration-500 w-full px-4 py-3"
+              className="border-2 rounded-md border-white bg-transparent text-white hover:bg-white hover:text-black transition-all duration-500 px-4 py-3"
             >
               {title}
               </Link>
-              </div>
           );
         })}
       </div>
